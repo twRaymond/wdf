057d5912fb52851ab6930e4c55223efb561569ac
raymond.lin <Raymond.Lin@acer.com>
2014-07-17 15:52:07 +0800

    modify resource path
    Signed-off-by: raymond.lin <Raymond.Lin@acer.com>

--- wdf/wdf-service/src/main/java/com/wdf/common/config/ConfigLoader.java
+++ wdf/wdf-service/src/main/java/com/wdf/common/config/ConfigLoader.java
@@ -1,25 +1,28 @@
 package com.wdf.common.config;
 
-import java.io.FileInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.Properties;
 
 public class ConfigLoader {
 	private static Properties prop = null;
-	private static String defaultName = "initConfig.properties";
+	private static String defaultName = "/initContext.properties";
 	
 	public void load(){
-		load(null);
+		load("");
 	}
 
 	public void load(String configName){
 		prop = new Properties();
-		InputStream input = null;
 		try {
-			input = new FileInputStream("".equalsIgnoreCase(configName)?defaultName:configName);
-			prop.load(input);
+			String tmpFileName = "".equalsIgnoreCase(configName)?defaultName:configName;
+			System.out.println("load file ... > " + tmpFileName );
+			InputStream resourcePath = getClass().getResourceAsStream(tmpFileName);
+			prop.load(resourcePath);
 		} catch(Exception e){
-			
+			System.out.println(e.toString());
 		}
 	}
 	
@@ -37,5 +40,9 @@ public class ConfigLoader {
 
 	public static String getKey(Properties resource, String key){
 		return resource.getProperty(key);
+	}
+	
+	public static void main(String args[]) {
+		System.out.println(ConfigLoader.class.getResource("/"));
 	}
 }
